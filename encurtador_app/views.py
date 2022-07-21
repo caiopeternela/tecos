46e43fe7c354775880281771a88714ad8922ba4d
Caio Peternela <caiopeternela.dev@gmail.com>
2022-07-21 15:37:08 -0300

    implementando o copy to clipboard

--- encurtador_app/views.py
+++ encurtador_app/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Url
+import pyperclip
 
 # Create your views here.
 def home(request):
     if request.method == "POST":
         url = request.POST.get('url')
         obj = Url.shorten(url)
+        short_url_link = 'tecos.cf' + '/' + obj.short_url
+        clipboard = pyperclip.copy(obj.short_url)
         return render(request, 'home.html', {
             'url': obj.url,
             'short_url': obj.short_url,
-            'short_url_link': 'tecos.cf' + '/' + obj.short_url
+            'short_url_link': short_url_link,
+            'clipboard': clipboard
         })
     return render(request, 'home.html')
 
