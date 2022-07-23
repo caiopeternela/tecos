ceb5f00140ac05ecb58a45e2f98b02f43b9399f8
Caio Peternela <caiopeternela.dev@gmail.com>
2022-07-23 16:24:33 -0300

    dale

--- encurtador_de_url/settings.py
+++ encurtador_de_url/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*s8jnr=#2im*368u-322^3**x1cz#59*5#5y-^gy*9*uu+k!il'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
